cc09955ec0f12515ff927fbbc2f3ac32d0f09884
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-12-03 02:16:05 +0900

    Add proxy.

--- chapter-02/proxy.py
+++ chapter-02/proxy.py
@@ -6,7 +6,7 @@ import socket
 import threading
 
 
-def server_loop(local_host, local_port, remote_host, receive_first):
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -64,5 +64,121 @@ def main():
     server_loop(local_host, local_port, remote_host, remote_port, receive_first)
 
 
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+
+    # リモートホストへの接続
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+
+    # 必要ならリモートホストからデータを受信
+    if receive_first:
+
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+
+        # 受信データ処理関数にデータ受け渡し
+        remote_buffer = response_handler(remote)
+
+        # もしローカル側に対して送るデータがあれば送信
+        if len(remote_buffer):
+            print('[<==] Sending %d bytes to localhost.' % len(remote_buffer))
+            client_socket.send(remote_buffer)
+    # ローカルからのデータ受信、リモートへの送信、ローカルへの送信
+    # の繰り返しを行うループ処理の開始
+    while True:
+
+        # ローカルホストからデータ受信
+        local_buffer = receive_from(client_socket)
+
+        if len(local_buffer):
+
+            print('[==>] Received %d bytes from localhost.' % len(local_buffer))
+            hexdump(local_buffer)
+
+            # 送信データ処理関数にデータ受け渡し
+            local_buffer = request_handler(local_buffer)
+
+            # リモートホストへのデータ送信
+            remote_socket.send(local_buffer)
+            print('[==>] Sent to remote.')
+
+            # 応答の受信
+            remote_buffer = receive_from(remote_socket)
+
+        if len(remote_buffer):
+            print('[<==] Received %d bytes from remote.' % len(remote_buffer))
+            hexdump(remote_buffer)
+
+            # 受信データ処理関数にデータ受け渡し
+            remote_buffer = response_handler(remote_buffer)
+
+            # ローカル側に応答データを送信
+            client_socket.send(remote_buffer)
+
+            print('[<==] Sent to localhost.')
+
+        # ローカル側・リモート側双方からデータが来なければ接続を閉じるsetuzokuwotojiru
+            if not len(local_buffer) or not len(remote_buffer):
+                client_socket.close()
+                remote_socket.close()
+                print('[*] No more data. Closing connections.')
+
+                break
+
+# 16進数ダンプを整形して表示する関数
+# 以下のURLのコメント欄から取得したコード
+# (折り返しバイト数の指定に関わるデフォルト引数の値のみ修正)
+# http://code.activestate.com/recipes/142812-hex-dumper/
+def hexdump(src, length=16):
+    result = []
+    digits = 4 if isinstance(src, unicode) else 2
+
+    for i in range(0, len(src), length):
+        s = src[i:i+length]
+        hexa = b' '.join(["%0*X" % (digits, ord(x)) for x in s])
+        text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.' for x in s])
+        result.append(b'%04X   %-*s   %s') % (i, length*(digits + 1), hexa,text)
+
+    print(b'\n'.join(result))
+
+def receive_from(connection):
+
+    buffer = b''
+
+    # タイムアウト値を2秒に設定(ターゲットに応じた調整が必要)
+    connection.settimeout(2)
+
+    try:
+        # データを受け取らなくなるかタイムアウトになるまでデータを受信して
+        # bufferに格納
+        while True:
+            data = connection.recv(4096)
+
+            if not data:
+                break
+
+            buffer += data
+
+    except:
+        pass
+
+    return buffer
+
+# リモート側のホストに送る全リクエストデータの改変
+def request_handler(buffer):
+    # パケットの改変を実施
+    return buffer
+
+
+# ローカル側のホストに送る全レスポンスデータの改変
+def response_handler(buffer):
+    # パケットの改変を実施
+    return buffer
+
 if __name__ == '__main__':
+    main()
+
+
+
+if __nae__ == '__main__':
     main()
